2fcc688a98c6d2e739b3dd26cc2cb948951bd6ee
Ricky2297 <rickysgb@gmail.com>
2020-10-03 17:54:55 +0000

    Excuse Generator Js

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,32 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let possetion = ["my homework", "my food", "my car", "my boat"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
